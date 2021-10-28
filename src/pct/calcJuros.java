74b14c70f573ba9f599067bf53601dfd94f59eeb
Denis Eduardo <89614090+iamghost99@users.noreply.github.com>
2021-10-28 21:57:53 -0200

    Projeto calculadora de juros e taxas

--- src/pct/calcJuros.java
+++ src/pct/calcJuros.java
@@ -15,30 +15,52 @@ import java.util.Scanner;
  */
 public class calcJuros {
     public static void main(String[] args) {
-      // Criação de variáveis
-        double c, i, n, j;
+        // Criação de variáveis
+        int opcao;
+        double c, i, n, j; //c = capital i = taxa n = capitalização k = juros
        
-       // Criação e instância do objeto de entrada
+        // Criação e instância do objeto de entrada
         Scanner entrada = new Scanner(System.in);
       
-      // Apresentação 
-        System.out.println("\n\t\t\t --- Calculando Juros --- \n");
-        
-     // Entradas
-        System.out.print("Digite o capital: ");
-        c = entrada.nextDouble();
-        System.out.print("Digite a taxa: ");
-        i = entrada.nextDouble();
-        System.out.print("Digite o perido de capitalização: ");
-        n = entrada.nextDouble();
-        
-     // Processamento
-        j = (i/100)* c * n;
-        
-     // Saída
-        //System.out.printf("\n\t\t(%.2f  / 100) * %.2f * %.2f = %.2f \n",i,c,n,j);
-        System.out.printf("O total de juros é de: R$ %.2f\n",j);
-                
-    }
-    
+        do{
+            // Apresentação 
+            System.out.println("\n\t --- Calculando de juros e taxas --- \n");
+            //menu
+            System.out.println("\n\t\t [1] Calcular juros");
+            System.out.println("\t\t [2] Calcular taxa");
+            System.out.println("\t\t [3] Sair....");
+            opcao = entrada.nextInt();
+            
+            switch(opcao){
+                    case 1:
+                        // Entradas
+                        System.out.print("Digite o capital: ");
+                        c = entrada.nextDouble();
+                        System.out.print("Digite a taxa: ");
+                        i = entrada.nextDouble();
+                        System.out.print("Digite o perido de capitalização: ");
+                        n = entrada.nextDouble();
+                        // Processamento
+                        j = (i/100)* c * n;
+                        // Saída        
+                        System.out.printf("O total de juros é de: R$ %.2f\n",j);
+                        break;
+                    case 2:
+                        // Entradas
+                        System.out.print("Digite o juros: ");
+                        j = entrada.nextDouble();
+                        System.out.print("Digite o capital: ");
+                        c = entrada.nextDouble();
+                        System.out.print("Digite o perido de capitalização: ");
+                        n = entrada.nextDouble();
+                        // Processamento
+                        i = (j / ( c * n )) * 100;
+                        // Saída        
+                        System.out.printf("A taxa é de R$ %.2f porcento\n", i);
+                        break;
+                    default:
+                        System.out.println("Opção invalida!!!!");
+            }
+        }while(opcao!=3);
+    }    
 }
